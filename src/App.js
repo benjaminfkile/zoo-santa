34a5406849141ece8b0f31b7cae84b030374ec86
ben <benjaminfkile@gmail.com>
2020-12-20 16:25:30 -0700

    endshow

--- src/App.js
+++ src/App.js
@@ -1,59 +1,59 @@
 import React, { Component } from 'react';
-import Aircraft from './Aircraft'
-import Map from './Map/Map'
-import Preshow from './Preshow/Preshow'
+// import Aircraft from './Aircraft'
+// import Map from './Map/Map'
+// import Preshow from './Preshow/Preshow'
 import EndShow from './EndShow/EndShow'
-import Sponsors from './Sponsors/Sponsors'
+// import Sponsors from './Sponsors/Sponsors'
 import './App.css';
 
 class App extends Component {
 
-  appMounted = false;
-  constructor(props) {
-    super(props);
-    this.state = {
-      aircraft: true,
-      showmode: "preshow",
-      preshowMsg: ""
-    }
-  }
+  // appMounted = false;
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     aircraft: true,
+  //     showmode: "endshow",
+  //     preshowMsg: ""
+  //   }
+  // }
 
-  componentDidMount() {
-    this.appMounted = true;
-    this.dbInterval = setInterval(this.listen4DB, 1000)
-    this.updateInterval = setInterval(this.update, 1000)
-  }
+  // componentDidMount() {
+  //   this.appMounted = true;
+  //   this.dbInterval = setInterval(this.listen4DB, 1000)
+  //   this.updateInterval = setInterval(this.update, 1000)
+  // }
 
-  componentWillUnmount() {
-    this.appMounted = false;
-    clearInterval(this.updateInterval)
-  }
+  // componentWillUnmount() {
+  //   this.appMounted = false;
+  //   clearInterval(this.updateInterval)
+  // }
 
-  listen4DB = () => {
-    if (Aircraft[0].data) {
-      this.setState({ aircraft: true })
-      this.update()
-      this.updateInterval = setInterval(this.update, 1000)
-      clearInterval(this.dbInterval)
-    }
-  }
+  // listen4DB = () => {
+  //   if (Aircraft[0].data) {
+  //     this.setState({ aircraft: true })
+  //     this.update()
+  //     this.updateInterval = setInterval(this.update, 1000)
+  //     clearInterval(this.dbInterval)
+  //   }
+  // }
 
-  update = () => {
-    try {
-      this.setState({
-        showmode: Aircraft[0].data.getElementsByTagName("showmode")[0].innerHTML,
-        preshowMsg: Aircraft[0].data.getElementsByTagName("preshow_msg")[0].innerHTML
-      })
-    } catch {
-      console.log('failed to set state')
-    }
-  }
+  // update = () => {
+  //   try {
+  //     this.setState({
+  //       showmode: Aircraft[0].data.getElementsByTagName("showmode")[0].innerHTML,
+  //       preshowMsg: Aircraft[0].data.getElementsByTagName("preshow_msg")[0].innerHTML
+  //     })
+  //   } catch {
+  //     console.log('failed to set state')
+  //   }
+  // }
 
   render() {
 
     return (
       <div className="App">
-        {this.state.showmode === "runshow" && <div className="Map_Wrapper">
+        {/* {this.state.showmode === "runshow" && <div className="Map_Wrapper">
           <Sponsors />
           <Map />
         </div>}
@@ -64,7 +64,8 @@ class App extends Component {
         </div>}
         {this.state.showmode === "endshow" && <div className="Endshow_Wrapper">
           <EndShow />
-        </div>}
+        </div>} */}
+        <EndShow/>
       </div>
     )
   }
